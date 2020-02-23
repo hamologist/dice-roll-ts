9eebf8fce0267100d1e45cc0d3a6812950b9d23f
Jordan Paris <hamologist@gmail.com>
2020-02-23 14:13:10 -0500

    Adds the ability to change the PORT for the application

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,8 @@ import express from "express";
 import { rollRouter } from "./controllers/roll";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use('/roll', rollRouter);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Dice Roll listening on port ${port}!`));
